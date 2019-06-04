d9192cdeeeb2b2efcd58ce9bdee4e15ef4d28495
Ron Onyonka <rononyonka@gmail.com>
2019-06-04 13:57:57 +0300

    fixed the post and put requests

--- app/views.py
+++ app/views.py
@@ -16,18 +16,17 @@ class ArticleView(APIView):
         return Response({"articles": serializer.data})
 
     def post(self, request):
-        article = request.data.get('article')
-
+        # data = request.data.get('article')
+        # article = get_object_or_404(Article.objects.all())
         # Create an article from the above data
-        serializer = ArticleSerializer(data=article)
+        serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            article_saved = serializer.save()
-        return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
+            serializer.save()
+        return Response(serializer.data)
 
     def put(self, request, pk):
         saved_article = get_object_or_404(Article.objects.all(), pk=pk)
-        data = request.data.get('article')
-        serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
+        serializer = ArticleSerializer(instance=saved_article, data=request.data, partial=True)
 
         if serializer.is_valid(raise_exception=True):
             article_saved = serializer.save()
